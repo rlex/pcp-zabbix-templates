8f9c99aef3c52aedb8b799e16192c53d251564ac
Lex Rivera <me@lex.io>
2016-02-27 23:57:42 +0300

    add discovery script

---
+++ pcp-discovery.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+case "$1" in
+
+  disk)
+    disks=($(cat /proc/diskstats | awk '{print $3}' | grep -v 'ram\|loop\|sr'))
+    counter=${#disks[@]}
+    echo "{"
+    echo -e "\t\"data\":[\n"
+    for (( i=0; ${counter}>i; i++ ))
+    do
+       if (( $i == $counter-1 )); then
+         echo -e "\t{ \"{#DISKDEV}\":\"${disks[$i]}\" }"
+       else
+         echo -e "\t{ \"{#DISKDEV}\":\"${disks[$i]}\" },"
+       fi
+    done
+    echo -e "\n\t]\n"
+    echo "}"
+  ;;
+
+  netif)
+    interfaces=($(ifconfig -a | sed 's/[ \t].*//;/^$/d'))
+    counter=${#interfaces[@]}
+    echo "{"
+    echo -e "\t\"data\":[\n"
+    for (( i=0; i<${counter}; i++ ))
+    do
+       if (( $i == $counter-1 )); then
+         echo -e "\t{ \"{#NETIF}\":\"${interfaces[$i]}\" }"
+       else
+         echo -e "\t{ \"{#NETIF}\":\"${interfaces[$i]}\" }",
+       fi
+    done
+    echo -e "\n\t]\n"
+    echo "}"
+  ;;
+
+  *)
+    echo "wrong argument"
+
+esac
